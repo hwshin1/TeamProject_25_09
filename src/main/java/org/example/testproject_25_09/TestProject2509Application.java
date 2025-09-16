618af6d1dc875c885d072ae0ed8cfdf13f2fbbea
hwshin1 <hwshin5044@gmail.com>
2025-09-16 11:51:42 +0900

    프로젝트 세팅

---
+++ src/main/java/org/example/testproject_25_09/TestProject2509Application.java
@@ -0,0 +1,13 @@
+package org.example.testproject_25_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject2509Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject2509Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/testproject_25_09/TestProject2509ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testproject_25_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProject2509ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
